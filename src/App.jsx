04f96f6caa9056c013531944d959c1848f17fdd9
stefanarctic <87850525+stefanarctic@users.noreply.github.com>
2024-05-01 17:01:56 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from 'react';
+
+import './scss/styles.scss';
+
+import Header from './components/Header';
+import About from './components/About';
+import Testimonials from './components/Testimonials';
+
+import * as DebugTools from './components/Util/DebugTools';
+import openURL from './components/Util/HideUrl';
+import initScrollAnimation from './components/Util/ScrollAnimation';
+import Services from './components/Services';
+import FAQ from './components/FAQ';
+import Call from './components/Call';
+import Footer from './components/Footer';
+
+
+function App() {
+
+  useEffect(() => {
+    initScrollAnimation();
+  }, []);
+
+  return (
+    <div className="App">
+      <Header />
+      <About />
+      <Testimonials />
+      <Services />
+      <FAQ />
+      <Call />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+
+const Main = () => {
+    return (
+        <main>
+            {/* <h1 className="hidden">BizHub - Your go-to for business management <br /> and communication</h1> */}
+            <h1 className="hidden">BizHub</h1>
+            <h3 className="hidden">Your go-to for business management and communication</h3>
+            <button className="hidden">Sign Up</button>
+        </main>
+    );
+}
+
+export default Main;
