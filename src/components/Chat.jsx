df5eca7205fbf77a0aa9893f4b5609e8fea95817
stefanarctic <aleluianu09@gmail.com>
2024-12-25 13:13:00 +0200

    Finished user management system

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,32 +1,34 @@
 import { useDispatch, useSelector } from "react-redux";
 import { getUsers, setUsers } from "../features/users";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import * as Utils from './Util/Utils';
 
 const Chat = () => {
 
     const dispatch = useDispatch();
-    const contacts = useSelector(state => state.contacts.value);
+    const users = useSelector(state => state.users.value);
 
     const updateUsers = async () => {
         const users = await getUsers();
-
-        console.log('Updated users: ', users);
+        console.log('update', users);
         dispatch(setUsers(users));
+    }
+    
+    getUsersGlobal = () => {
+        return users;
     }
 
     useEffect(() => {
-
-        (async () => {
-            await updateUsers();
-            console.log
-        })();
-
+        updateUsers();
     }, []);
 
     return (
-        <></>
+        <div className="test">
+            { users && users.map(user => (
+                <p key={Utils.generateRandomId()}>{user.email}</p>
+            )) }
+        </div>
     );
-
 }
 
 export default Chat;
--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { configureStore } from '@reduxjs/toolkit'
-import contactReducer from './features/users.js'
+import userReducer from './features/users.js'
 import { Provider } from 'react-redux'
 
 const store = configureStore({
   reducer: {
-    contacts: contactReducer
+    users: userReducer
   }
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Provider store={store} >
       <App />
     </Provider>
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
